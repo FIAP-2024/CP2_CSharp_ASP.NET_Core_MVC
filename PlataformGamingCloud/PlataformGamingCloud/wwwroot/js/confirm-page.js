b884f5c33784521323b71043f9d8135a017a8c26
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-05-08 13:03:22 -0300

    feat: Implementado a janela de confirmação ao clicar em remover jogo.

---
+++ PlataformGamingCloud/PlataformGamingCloud/wwwroot/js/confirm-page.js
@@ -0,0 +1,10 @@
+﻿let buttonRemove = document.getElementsByClassName("button-link-remover");
+let confirmOverlay = document.getElementsByClassName("confirm-overlay");
+
+function showConfirmOverlay() {
+    confirmOverlay[0].style.display = "flex";
+}
+
+function hideConfirmOverlay() {
+    confirmOverlay[0].style.display = "none";
+}
