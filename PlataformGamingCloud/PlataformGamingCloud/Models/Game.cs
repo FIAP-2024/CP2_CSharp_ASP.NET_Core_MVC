191503ef9866d856381fbfaf9ed9f338bd85ec33
marceloamellopaixao <marceloamellopaixao@gmail.com>
2024-05-10 04:57:12 -0300

    fix: Implementada na classe game o dado DataLancamento

--- PlataformGamingCloud/PlataformGamingCloud/Models/Game.cs
+++ PlataformGamingCloud/PlataformGamingCloud/Models/Game.cs
@@ -9,11 +9,12 @@ public class Game
     public int GameId { get; set; }
     public string? Nome { get; set; }
     public string? Descricao { get; set; }
+    public DateOnly DataLancamento { get; set; }
     public DateTime DataCreated { get; set; } = DateTime.Now;
 
     [Display(Name ="Gênero")]
     public Genero? Genero { get; set; }
-    public double? Valor { get; set; }
+    public decimal? Valor { get; set; }
 }
 
 public enum Genero
